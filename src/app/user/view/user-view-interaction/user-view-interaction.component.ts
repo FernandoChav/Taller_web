c72172ddf068dea0ad67d9f1314b5a2a196f07fc
bxnjadev <benjamin.hyx@gmail.com>
2024-12-29 14:00:32 -0300

    feat: added a way for search users by name :tada:

--- src/app/user/view/user-view-interaction/user-view-interaction.component.ts
+++ src/app/user/view/user-view-interaction/user-view-interaction.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { UserController } from '../controller/user.controller';
+import { UserService } from '../../service/user.service';
+import { HttpHeaderUtil } from '../../../util/http.header.util';
 
 @Component({
   selector: 'app-user-view-interaction',
@@ -10,7 +12,9 @@ import { UserController } from '../controller/user.controller';
 })
 export class UserViewInteractionComponent {
 
-  constructor(public controller : UserController) {}
+  constructor(public controller : UserController,
+    private userService : UserService
+  ) {}
 
   nextPage() {
 
@@ -21,7 +25,19 @@ export class UserViewInteractionComponent {
   }
 
   onSearch(event : Event) {
+      this.controller.resetPage();
+      
+      const input = event.target as HTMLInputElement; 
+      this.controller.searchByName = input.value;
 
+      console.log("HOLA");
+
+      this.userService.query(
+          this.controller.parameters(), HttpHeaderUtil.asBearToken("token")
+      ).forEach(next => {
+        console.log("QUERYY");
+        this.controller.group = next;
+      }, );
   }
 
 }
