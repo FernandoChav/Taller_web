3b0b4c1c8210bd42fa2fb08838e12bf3bfbe765f
bxnjadev <benjamin.hyx@gmail.com>
2025-01-03 00:35:06 -0300

    feat: added form for update user

---
+++ src/app/user-panel/user-panel-edit/user-panel-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-panel-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-panel-edit.component.html',
+  styleUrl: './user-panel-edit.component.css'
+})
+export class UserPanelEditComponent {
+
+}
--- src/app/user/view/controller/user.controller.ts
+++ src/app/user/view/controller/user.controller.ts
@@ -3,11 +3,12 @@ import { User } from "../../interface/user";
 import { EntityGroup } from "../../../entities/entity.group";
 import { Paginable } from "../../../util/paginable";
 import { ObjectParameters } from "../../../entities/object.parameters";
+import { EntityServiceSharedPaginable } from "../../../service/entity.service.shared.paginable";
 
 @Injectable({
     providedIn: 'root'
 })
-export class UserController implements Paginable {
+export class UserController implements EntityServiceSharedPaginable<User> {
     page: number = 1;
     searchByName : string = "";
     group? : EntityGroup<User> | null = null;
