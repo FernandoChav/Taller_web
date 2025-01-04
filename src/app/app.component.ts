175ccefdaa3d7f8c0e7c60b942d4fff5548b0e8d
bxnjadev <benjamin.hyx@gmail.com>
2025-01-04 13:13:24 -0300

    feat: added componet for page not found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,15 @@ import { ProductViewComponent } from './product/view/product-view/product-view.c
 import { ShoppingCartViewComponent } from './shopping-cart/shopping-cart-view/shopping-cart-view.component';
 import { UserViewComponent } from './user/view/user-view/user-view.component';
 import { UserPanelViewComponent } from './user-panel/user-panel-view/user-panel-view.component';
+import { NotFoundViewComponent } from './not-found/view/not-found-view/not-found-view.component';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, 
     ProductViewComponent,
     ShoppingCartViewComponent,
     UserViewComponent, 
-    UserPanelViewComponent
+    UserPanelViewComponent,
+    NotFoundViewComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
